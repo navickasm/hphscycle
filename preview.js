88267a1b797c851a6be4705000b82f16edf144b3
navickasm <261navickasm@student.dist113.org>
2023-11-02 10:45:10 -0500

    stackblitz fix

---
+++ preview.js
@@ -0,0 +1,19 @@
+const THE_FILE = "builder.html";
+
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+
+//app.use(express.static(path.join(__dirname, "/")));
+
+app.get("*", (req, res) => {
+  console.log("req.path", req.path);
+  res.sendFile(path.join(__dirname + "/" + THE_FILE));
+});
+
+app.set("port", 4000);
+
+let server = http.createServer(app);
+server.listen(4000);
