b9764f20e29d6da2e5de41eb594f82495679451e
null <null>
2024-01-10 12:16:35 -0600

    valentine prelim

--- web/colorgetter.js
+++ web/colorgetter.js
@@ -1,8 +1,20 @@
 document.documentElement.style.setProperty("--light", localStorage.getItem("lightcolor"));
-  document.documentElement.style.setProperty("--main", localStorage.getItem("maincolor"));
-  document.documentElement.style.setProperty("--link", localStorage.getItem("linkcolor"));
-  document.documentElement.style.setProperty("--othertext", localStorage.getItem("othertextcolor"));
-  document.documentElement.style.setProperty("--bg", localStorage.getItem("bgcolor"));
-  document.documentElement.style.setProperty("--text", localStorage.getItem("textcolor"));
-  document.documentElement.style.setProperty("--border", localStorage.getItem("bordercolor"));
-  document.documentElement.style.setProperty("--pn", localStorage.getItem("pncolor"));
+document.documentElement.style.setProperty("--main", localStorage.getItem("maincolor"));
+document.documentElement.style.setProperty("--link", localStorage.getItem("linkcolor"));
+document.documentElement.style.setProperty("--othertext", localStorage.getItem("othertextcolor"));
+document.documentElement.style.setProperty("--bg", localStorage.getItem("bgcolor"));
+document.documentElement.style.setProperty("--text", localStorage.getItem("textcolor"));
+document.documentElement.style.setProperty("--border", localStorage.getItem("bordercolor"));
+document.documentElement.style.setProperty("--pn", localStorage.getItem("pncolor"));
+
+function valentine() {
+
+  document.documentElement.style.setProperty("--light",     "#732b41");
+  document.documentElement.style.setProperty("--main",      "#d84896");
+  document.documentElement.style.setProperty("--link",      "#000000");
+  document.documentElement.style.setProperty("--othertext", "#e5c2d5");
+  document.documentElement.style.setProperty("--bg",        "#a25787");
+  document.documentElement.style.setProperty("--text",      "#e8e8e8");
+  document.documentElement.style.setProperty("--border",    "#000000");
+  document.documentElement.style.setProperty("--pn",        "#000000");
+}
